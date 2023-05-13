b2933c30e9cacded5b6d8fd159e92ec4b99e9204
JonathanRodBar <jonathan.rodr.barja@gmail.com>
2023-05-13 11:05:16 +0200

    dummy move def

--- files/dummy_move.lua
+++ files/dummy_move.lua
@@ -8,26 +8,31 @@ end
 function best_move(me)
     local me_pos = me:pos()
 
-    local min_distance = 100
+    local min_distance = 10000000
     local closest_enemy = nil
 
     for _, player in ipairs(me:visible()) do
+        print("chekpoint 1")
         local dist = vec.distance(me_pos, player:pos())
         if dist < min_distance then
+            print("chekpoint 2")
             min_distance = dist
             closest_enemy = player
         end
     end
     if closest_enemy == nil then
+        print("No enemy found")
         return vec.new(0, 0)
     end
-    return me_pos:sub(closest_enemy:pos())
+    print("Enemy found")
+    return closest_enemy:pos():sub(me_pos):neg()
 
 end
 
 -- Main bot function
 function bot_main(me)
     local best_move = best_move(me)
+
     
     -- Move towards the target
     me:move(best_move)
